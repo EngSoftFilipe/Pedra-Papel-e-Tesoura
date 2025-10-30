0b33c459f09bf53d1199bf6a064d21dee3c079fa
Filipe <223964940+EngSoftFilipe@users.noreply.github.com>
2025-10-29 22:07:33 -0300

    Update Jokempo.java

--- Jokempo.java
+++ Jokempo.java
@@ -1,67 +1,95 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class Jokempô{
+public class Jokempo{
     public static void main(String[] args){
-            Scanner scanner = new Scanner(System.in);
-                    System.out.printf("\nJokempô\n\n");
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("Qual o seu nome?\n\n");
-        String name = scanner.nextLine();
+            System.out.printf("\n===== Jokempô =====\n\n");
 
-        System.out.printf("\nOlá, " + name + ", Escolha uma opção: ");
-        System.out.println("\n\n1- Pedra");
+        System.out.print("Digite seu nome: ");
+            String name = scanner.nextLine();
+
+            System.out.println("\nOlá " + name + ", Escolha uma das opções abaixo\n");
+
+    int jogador;
+    
+    do{
+        System.out.println("\n1- Pedra");
         System.out.println("2- Papel");
-        System.out.println("3- Tesoura\n");
+        System.out.println("3- Tesoura");
+
+        System.out.println("\n0- Sair\n\n");
+
+        System.out.print("Opção escolhida: ");
         
-            System.out.print("Opção escolhida: ");
+        jogador = scanner.nextInt();
 
-            int Jogador = scanner.nextInt();
-
-        switch (Jogador){
-            case 1:
-                System.out.printf("\n" + name + ": Pedra\n");
-                break;
+            switch (jogador){
+                case 1:
+                    System.out.println("\n" + name + " Pedra");
+                        break;
 
                 case 2:
-                    System.out.printf("\n" + name + ": Papel\n");
-                    break;
-
-            case 3:
-                System.out.printf("\n" + name + ": Tesoura\n");
-                break;
-                    default:
-                        System.out.println("Opção inválida");
-                            System.exit(0);
-        }
-
-                Random random = new Random();
-                int Bot = random.nextInt(3) + 1;
+                    System.out.println("\n" + name + " Papel");
+                        break;
                 
+                case 3:
+                    System.out.println("\n" + name + " Tesoura");
+                        break;
 
-        switch (Bot){
-            case 1:
-                System.out.println("Computador: Pedra");
-                break;
+                case 0:
+                    System.out.println("\n Saindo...\n");
+                        break;
 
+                default:
+                    System.out.println("Opção inválida, tente novamente!");
+                    continue;
+        }
+
+        if (jogador == 0) break;
+        
+            Random random = new Random();
+            int Bot = random.nextInt(3) + 1;
+
+            switch (Bot){
+                case 1:
+                    System.out.println("Bot: Pedra");
+                        break;
                 case 2:
-                    System.out.println("Computador: Papel");
-                    break;
+                    System.out.println("Bot: Papel");
+                        break;
+                case 3:
+                    System.out.println("Bot: Tesoura");
+                        break;
 
-            case 3:
-                System.out.println("Computador: Tesoura");
-                break;
-        }
+                case 0:
+                    System.out.println(" ");
+                        break;
 
-            if (Jogador == Bot){
-                System.out.println("\nEmpate!\n\n");
-            } else if ((Jogador == 1 /*Pedra*/) && (Bot == 3 /*Tesoura*/) ||
-                        (Jogador == 2 /*Papel*/) && (Bot == 1 /*Pedra*/) ||
-                         (Jogador == 3 /*Tesoura*/) && (Bot == 2 /*Papel*/)){
-                            System.out.printf("\nParabéns, " + name + ", você venceu!\n\n");
-                         } else{
-                            System.out.printf("\nVocê perdeu!\n\n");
-        }
-            scanner.close();
+                    default:
+                        System.out.println(" ");
+            }       
+
+        if (jogador == Bot){
+            System.out.print("\nEmpate!\n\n");
+
+        } else if ((jogador == 1) && (Bot == 3) ||
+                        (jogador == 2) && (Bot == 1) ||
+                          (jogador == 3) && (Bot == 2)){
+                            System.out.print("\nParabéns " + name + ", você venceu!!\n");
+                          }
+
+         else if ((Bot == 1) && (jogador == 3) ||
+                        (Bot == 2) && (jogador == 1) ||
+                          (Bot == 3) && (jogador == 2)){
+                            System.out.print("\nVocê perdeu!!\n");
+                          }
+                          
+                } 
+                    while (jogador != 0);
+
+        scanner.close();
+        
     }
 }
